02211e6354c9bcbf5a67b54dfa99892a1c504c54
JoaoMainardes <JoaoMainardes@users.noreply.github.com>
2018-07-05 09:57:51 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,146 @@
+<?php
+// These lines are mandatory.
+require_once 'Mobile_Detect.php';
+$detect = new Mobile_Detect;
+?>
+<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+		<title>Joao Mainardes - Digital Marketing Specialist</title>
+
+		<meta name="description" content="Personal site of Joao Mainardes, Digital Marketing Specialist in Miami."/>
+		<meta property="og:locale" content="en_US" />
+		<meta property="og:type" content="website" />
+		<meta property="og:title" content="Joao Mainardes" />
+		<meta property="og:description" content="Personal site of Joao Mainardes, Digital Marketing Specialist in Miami." />
+		<meta property="og:url" content="https://joaomainardes.com" />
+		<meta property="og:site_name" content="Joao Mainardes" />
+		<meta property="og:image" content="https://joaomainardes.com/img/preview.jpg" />
+		<meta name="twitter:card" content="summary_large_image">
+		<meta name="twitter:site" content="@joaomainardes">
+		<meta name="twitter:creator" content="@joaomainardes">
+		<meta name="twitter:title" content="Joao Mainardes">
+		<meta name="twitter:description" content="Digital Marketing Specialist in Miami.">
+		<meta name="twitter:image" content="https://joaomainardes.com/img/preview.jpg">
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+    
+<?php
+// Check for mobile environment.
+if ($detect->isMobile()) {
+    
+    <link rel="stylesheet" href="css/style-mobile.css" />
+			</head>
+
+	<body>
+		<section id="content">
+
+			<div id="scrollable">
+
+				<section class="description">
+					<h1><span class="line" style="visibility: visible;"><span class="line__value" style="visibility: visible;">Joao Mainardes</span><span class="line__bg" data-properties="[object Object]" style="-webkit-transform: translate(101%, 0px);"></span></span></h1>
+
+					<p>
+						<span class="line" style="visibility: visible;"><span class="line__value" style="visibility: visible;">Award-winning digital marketing specialist, designer & developer.</span><span class="line__bg" data-properties="[object Object]" style="-webkit-transform: translate(101%, 0px);"></span></span><br />
+						<span class="line" style="visibility: visible;"><span class="line__value" style="visibility: visible;">Born in Brazil, raised in Chile and currently living in Miami, USA.</span><span class="line__bg" data-properties="[object Object]" style="-webkit-transform: translate(101%, 0px);"></span></span><br />
+						<span class="line" style="visibility: visible;"><span class="line__value" style="visibility: visible;">Founder & Creative Director @ <a href="https://crevtive.com/" target="_blank">@ CREVTIVE</a></span><span class="line__bg" data-properties="[object Object]" style="-webkit-transform: translate(101%, 0px);"></span></span><br /><br />
+						<span class="line" style="visibility: visible;"><span class="line__value" style="visibility: visible;">For any project inquiry, feel free</span><span class="line__bg" data-properties="[object Object]" style="-webkit-transform: translate(101%, 0px);"></span></span><br />
+						<span class="line" style="visibility: visible;"><span class="line__value" style="visibility: visible;">to say hello at <a href="mailto:joao@crevtive.com">joao@crevtive.com</a></span><span class="line__bg" data-properties="[object Object]" style="-webkit-transform: translate(101%, 0px);"></span></span><br /><br />
+						<span class="line" style="visibility: visible;"><span class="line__value" style="visibility: visible;">
+							<a href="https://www.linkedin.com/in/joaomainardes" target="_blank">LinkedIn</a> <span class="sep">•</span>
+							<a href="https://twitter.com/joaomainardes" target="_blank">Twitter</a> <span class="sep">•</span>
+							<a href="https://instagram.com/joaomainardes" target="_blank">Instagram</a>
+						</span><span class="line__bg" data-properties="[object Object]" style="-webkit-transform: translate(101%, 0px);"></span></span>
+					</p>
+				</section>
+			</div>
+
+			<span class="mask" data-properties="[object Object]" style="-webkit-transform: translate(100%, 0px);"></span>
+
+			<div class="background" data-properties="[object Object]" style="opacity: 1; -webkit-transform: translate(0px, 0px);"></div>
+
+			<a class="lab-link c-v__child" href="#" target="_blank"></a>
+		</section>
+</body>
+    
+}
+
+<?php
+// Check for mobile environment.
+if ($detect->isTablet()) {
+    
+    <link rel="stylesheet" href="css/style-mobile.css" />
+			</head>
+
+	<body>
+		<section id="content">
+
+			<div id="scrollable">
+
+				<section class="description">
+					<h1><span class="line" style="visibility: visible;"><span class="line__value" style="visibility: visible;">Joao Mainardes</span><span class="line__bg" data-properties="[object Object]" style="-webkit-transform: translate(101%, 0px);"></span></span></h1>
+
+					<p>
+						<span class="line" style="visibility: visible;"><span class="line__value" style="visibility: visible;">Award-winning digital marketing specialist, designer & developer.</span><span class="line__bg" data-properties="[object Object]" style="-webkit-transform: translate(101%, 0px);"></span></span><br />
+						<span class="line" style="visibility: visible;"><span class="line__value" style="visibility: visible;">Born in Brazil, raised in Chile and currently living in Miami, USA.</span><span class="line__bg" data-properties="[object Object]" style="-webkit-transform: translate(101%, 0px);"></span></span><br />
+						<span class="line" style="visibility: visible;"><span class="line__value" style="visibility: visible;">Founder & Creative Director @ <a href="https://crevtive.com/" target="_blank">@ CREVTIVE</a></span><span class="line__bg" data-properties="[object Object]" style="-webkit-transform: translate(101%, 0px);"></span></span><br /><br />
+						<span class="line" style="visibility: visible;"><span class="line__value" style="visibility: visible;">For any project inquiry, feel free</span><span class="line__bg" data-properties="[object Object]" style="-webkit-transform: translate(101%, 0px);"></span></span><br />
+						<span class="line" style="visibility: visible;"><span class="line__value" style="visibility: visible;">to say hello at <a href="mailto:joao@crevtive.com">joao@crevtive.com</a></span><span class="line__bg" data-properties="[object Object]" style="-webkit-transform: translate(101%, 0px);"></span></span><br /><br />
+						<span class="line" style="visibility: visible;"><span class="line__value" style="visibility: visible;">
+							<a href="https://www.linkedin.com/in/joaomainardes" target="_blank">LinkedIn</a> <span class="sep">•</span>
+							<a href="https://twitter.com/joaomainardes" target="_blank">Twitter</a> <span class="sep">•</span>
+							<a href="https://instagram.com/joaomainardes" target="_blank">Instagram</a>
+						</span><span class="line__bg" data-properties="[object Object]" style="-webkit-transform: translate(101%, 0px);"></span></span>
+					</p>
+				</section>
+			</div>
+
+			<span class="mask" data-properties="[object Object]" style="-webkit-transform: translate(100%, 0px);"></span>
+
+			<div class="background" data-properties="[object Object]" style="opacity: 1; -webkit-transform: translate(0px, 0px);"></div>
+
+			<a class="lab-link c-v__child" href="#" target="_blank"></a>
+		</section>
+</body>
+    
+}
+
+<php }else{ ?>
+<link rel="icon" href="img/favicon.png">
+
+				<link rel="stylesheet" href="css/style.css">
+			</head>
+
+	<body>
+		<section id="content">
+
+			<div id="scrollable">
+
+				<section class="description">
+					<h1><span class="line">Joao Mainardes</span></h1>
+
+					<p>
+						<span class="line">Award-winning digital marketing specialist, designer & developer.</span><br>
+						<span class="line">Born in Brazil, raised in Chile and currently living in Miami, USA.</span><br>
+						<span class="line">Founder & Creative Director <a href="https://www.crevtive.com/" target="_blank">@ CREVTIVE</a></span><br><br>
+						<span class="line">For any project inquiry, feel free</span><br>
+						<span class="line">to say hello at <a href="mailto:joao@crevtive.com">joao@crevtive.com</a></span><br><br>
+						<span class="line">
+							<a href="https://www.linkedin.com/in/joaomainardes" target="_blank">LinkedIn</a> <span class="sep">•</span>
+							<a href="https://twitter.com/joaomainardes" target="_blank">Twitter</a> <span class="sep">•</span>
+							<a href="https://instagram.com/joaomainardes" target="_blank">Instagram</a>
+						</span>
+					</p>
+				</section>
+			</div>
+
+			<span class="mask"></span>
+
+			<div class="background"></div>
+
+			<a class="lab-link c-v__child" href="#" target="_blank"></a>
+		</section>
+
+	</body>
+		<script type="text/javascript" src="js/scripts.js"></script>
+<?php } ?>
